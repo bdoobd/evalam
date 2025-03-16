b5938e063834271472a4245c6d3de3a076bab5ce
bdoobd <G3xAg0N@mail.ru>
2025-03-16 22:25:17 +0200

    FIX: Перенос действий в меню вместо кнопок

--- app/models/stock.py
+++ app/models/stock.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import DateTime
+from sqlalchemy import DateTime, text
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.dao.db_base import Base, str_req, str_req_uq
@@ -14,6 +14,7 @@ class Stock(Base):
         DateTime(timezone=True), default=datetime.now
     )
     consignor: Mapped[str_req]
+    ready: Mapped[bool] = mapped_column(server_default=text("FALSE"))
     note: Mapped[str | None]
 
     items: Mapped[list["Item"]] = relationship(
--- app/static/js/views/item.js
+++ app/static/js/views/item.js
@@ -5,7 +5,8 @@ class Item {
   render() {
     this._parentElement.insertAdjacentHTML(
       "afterbegin",
-      this.generateStockMarkup()
+      // this.generateStockMarkup()
+      this._modalMarkup()
     );
   }
   generateStockMarkup() {
@@ -26,6 +27,36 @@ class Item {
       console.log(addButton);
       handler();
     });
+  }
+
+  _modalMarkup() {
+    return `<div class="modal-base">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="staticBackdropLabel">Select stock ref</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+             <form id="stockForm" onsubmit="processForm(event)">
+                <div class="form-check mb-3">
+                  <input class="form-check-input" type="radio" name="stock-ref" id="new-stock">
+                  <label class="form-check-label" for="new-stock">
+                    Add new stock
+                  </label>
+                </div>
+                <div class="form-check mb-3">
+                  <input class="form-check-input" type="radio" name="stock-ref" id="select-stock" checked>
+                  <label class="form-check-label" for="select-stock">
+                    Select existing stock
+                  </label>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+            </div>
+        </div>
+    </div>
+</div>`;
   }
 }
 
